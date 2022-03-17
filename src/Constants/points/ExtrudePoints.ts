559cee8d1d2b7eee0c873fc6bb718b1152851ead
Srikanta-ku-Mishra <srikantamishra@evawinoptimize.com>
2022-03-17 17:53:16 +0530

    Adding Extrude Setting

---
+++ src/Constants/points/ExtrudePoints.ts
@@ -0,0 +1,45 @@
+
+
+export const ExtrudePoints = [
+    {
+        lineX1: {
+            point1: {
+                x: -2.5,
+                y: 1.3,
+          
+            },
+
+            point2: {
+                x: -1.5,
+                y: 1.3,
+              
+            }
+        },
+        lineX2: {
+            point1: {
+                x: -1.5,
+                y: 1.3,
+               
+            },
+
+            point2: {
+                x: -1.5,
+                y: -0.3,
+               
+            }
+        },
+        lineX3: {
+            point1: {
+                x: -1.5,
+                y: -0.3,
+              
+            },
+
+            point2: {
+                x: -2.5,
+                y: -0.3,
+              
+            }
+        },
+    }
+]
